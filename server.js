4a050e708382a0bbc72f4edb63d79a1c094d5f36
Michele Marschner <michele.marschner@gmail.com>
2020-07-21 08:43:38 +0200

    aded post request

--- server.js
+++ server.js
@@ -8,36 +8,36 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-
 app.get('/api/quotes', (req, res, next) => {
-
-   
-        const author = req.query.person;
-        if (author) {
-            const quotesByPerson = quotes.filter(quote => {
-            return quote.person === author;
-            })
-            res.json({quote: {quotesByPerson}});
-        } else {
-            //res.status(404).send({msg: "Author does not exists"});
-            res.json({quotes});
-        }
+    const author = req.query.person;
+    if (author) {
+        const quotesByPerson = quotes.filter(quote => {
+        return quote.person === author;
+        })
+        res.json({quotes: quotesByPerson});
+    } else {
+        //res.status(404).send({msg: "Author does not exists"});
+        res.json({quotes});
+    }
 });
 
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = getRandomElement(quotes);
-    res.json({quote: {randomQuote}});
+    res.json({quote: randomQuote});
 });
 
 app.post('/api/quotes', (req, res, next) => {
-    const newQuote = req
-
-
-    const randomQuote = getRandomElement(quotes);
-    res.json({quote: {randomQuote}});
+    const newQuote = {quote: req.query.quote, person: req.query.person};
+    if(newQuote.quote && newQuote.person) {
+        quotes.push(newQuote);
+        res.status(201).json({quote: newQuote});
+    } else {
+        res.status(400).json();
+    }
+    
 });
 
-
+//add put & delete --> needs id on quotes
 
 app.listen(PORT);
 
